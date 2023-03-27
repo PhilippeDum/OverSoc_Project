225dfe2143dd2f1248586626a51b3df9dfdf004d
PhilippeDum <phil.dum88@gmail.com>
2023-03-27 03:39:26 +0200

    [UPDATE] Add Start of List View + Elements Management in UI_Manager.cs + Add Delete part (texts + progress bar + delete button)

--- Assets/UI/UI_Manager.cs
+++ Assets/UI/UI_Manager.cs
@@ -1,35 +1,150 @@
+using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.UIElements;
 
 public class UI_Manager : MonoBehaviour
 {
+    [SerializeField] private VisualTreeAsset elementTemplate;
+
+    [SerializeField] private List<string> characters;
+
     UIDocument uiDocument;
 
+    private TextField textField;
+
     private Button headerButton;
+    private Button addButton;
+    private Button deleteButton;
+
+    private Label limitLabel;
+    private int maxLimit = 20;
+    private int currentLimit;
+
+    private ProgressBar progressBar;
+
+    private ListView listView;
+    private VisualElement selectedElement;
 
     private void Start()
     {
         uiDocument = GetComponent<UIDocument>();
 
-        headerButton = uiDocument.rootVisualElement.Q("Header-Button") as Button;
+        textField = uiDocument.rootVisualElement.Q("Add-TextField") as TextField;
 
+        headerButton = uiDocument.rootVisualElement.Q("Header-Button") as Button;
         headerButton.RegisterCallback<ClickEvent>(Quit);
-        //SetupButton(headerButton);
+
+        addButton = uiDocument.rootVisualElement.Q("Add-Button") as Button;
+        addButton.RegisterCallback<ClickEvent>(AddElement);
+
+        deleteButton = uiDocument.rootVisualElement.Q("Delete-Button") as Button;
+        deleteButton.RegisterCallback<ClickEvent>(RemoveElement);
+
+        currentLimit = characters.Count;
+
+        limitLabel = uiDocument.rootVisualElement.Q("Limit-Texts-Numbers") as Label;
+        limitLabel.text = $"{currentLimit} / {maxLimit}";
+
+        progressBar = uiDocument.rootVisualElement.Q("CustomProgressBar") as ProgressBar;
+        progressBar.highValue = maxLimit;
+        progressBar.value = currentLimit;
+        
+        HandleList();
     }
 
-    /*private void SetupButton(VisualElement button)
+    private void Update()
     {
-        var buttonIcon = button.Q(name: "Header-Button-Icon");
-        var iconPath = "Icons/Vector";
-        var iconAsset = Resources.Load<Texture2D>(iconPath);
-
-        buttonIcon.style.backgroundImage = iconAsset;
-
-        button.RegisterCallback<ClickEvent>(Quit);
-    }*/
+        if (selectedElement == null)
+        {
+            deleteButton.SetEnabled(false);
+        }
+        else
+        {
+            deleteButton.SetEnabled(true);
+        }
+    }
 
     private void Quit(ClickEvent evt)
     {
-        Debug.Log($"Call event : {evt}");
+        Debug.Log($"Quit Application");
+    }
+
+    private void AddElement(ClickEvent evt)
+    {
+        if (textField == null || listView == null) return;
+
+        Debug.Log($"Add {textField.text}");
+
+        var template = elementTemplate.Instantiate();
+        var customElement = template.Q("CustomElement");
+        Label label_Char = customElement.Q("Name") as Label;
+        label_Char.text = textField.text;
+
+        characters[characters.Count] = textField.text;
+        uiDocument.rootVisualElement.Q<ListView>("Content").RefreshItems();
+    }
+
+    private void RemoveElement(ClickEvent evt)
+    {
+        if (selectedElement == null || listView == null) return;
+
+        Debug.Log($"Remove {selectedElement} - {selectedElement.parent}");
+        selectedElement.RemoveFromHierarchy();
+        listView.Remove(selectedElement);
+        listView.RefreshItems();
+    }
+
+    private VisualElement SetupCustomElement(VisualTreeAsset elementTemplate)
+    {
+        var template = elementTemplate.Instantiate();
+        var customElement = template.Q("CustomElement");
+        customElement.RegisterCallback<ClickEvent>(SelectElement);
+        return template;
+    }
+
+    private void SelectElement(ClickEvent evt)
+    {
+        selectedElement = (VisualElement)evt.currentTarget;
+    }
+
+    private void HandleList()
+    {
+        var customList = new List<string>(characters.Count);
+
+        listView = uiDocument.rootVisualElement.Q<ListView>("Content");
+
+        listView.makeItem = () =>
+        {
+            return SetupCustomElement(elementTemplate);
+        };
+
+        listView.bindItem = (element, index) =>
+        {
+            element.name = $"Element_{index}";
+
+            Label label_Pos = element.Q("ID") as Label;
+            label_Pos.text = $"{index + 1}";
+
+            Label label_Char = element.Q("Name") as Label;
+            label_Char.text = characters[index];
+
+            var elementIcon = element.Q("ElementIcon");
+
+            var iconPath = "Icons/Vector_List";
+            var iconAsset = Resources.Load<Texture2D>(iconPath);
+
+            elementIcon.style.backgroundImage = iconAsset;
+        };
+
+        listView.itemsSource = characters;
+        listView.selectionType = SelectionType.Multiple;
+        listView.fixedItemHeight = 52;
+        listView.onItemsChosen += OnItemsChosen;
+    }
+
+    private void OnItemsChosen(IEnumerable<object> objets)
+    {
+        Debug.Log($"{nameof(OnItemsChosen)}()");
     }
 }
